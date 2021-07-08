2feae4a384820161406b6b4db0227a6bb263b216
Eduardo Garcia <al221811274@gmail.com>
2021-07-07 19:04:01 -0500

    Crud de procesos al 50%

---
+++ app/Http/Controllers/ProcesosController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Requests\ProcesosRequest;
+use App\Models\Proceso;
+
+class ProcesosController extends Controller
+{
+    public function index(){
+        $procesos = Proceso::paginate();
+        return view ('system.procesos.index',compact('procesos'));
+    }
+
+    public function create(){
+        return view ('system.procesos.create');
+    }
+
+    public function store(ProcesosRequest $request)
+    {
+        $proceso = Proceso::create($request->validated());
+                    return redirect()
+                    ->route('procesos.index')
+                    ->withSuccess("El proceso $proceso->nombre ha sido creado exitosamente");
+    }
+
+    public function edit(Proceso $procesos)
+    {
+        return view('system.procesos.edit', [
+            'procesos' => $procesos,
+        ]);
+    }
+
+    public function update(ProcesosRequest $request, procesos $procesos)
+    {
+        $procesos->update($request->validated());
+        $procesos->save();
+        return redirect()
+                ->route('procesos.index')
+                ->withSuccess("El proceso $procesos->nombre ha sido modificado exitosamente");
+    }
+}
+
+
--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,10 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\InicioController;
 use App\Http\Controllers\ProductosController;
 use App\Http\Controllers\ClientesController;
+use App\Http\Controllers\ProcesosController;
 
 Route::get('/', [InicioController::class,'Inicio'])->name('Inicio');
 
 Route::resource('productos', ProductosController::class);
 Route::resource('clientes', ClientesController::class);
+Route::resource('procesos', ProcesosController::class);
