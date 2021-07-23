145ca5fab2d179439eadb9065f97ecb824e1128e
Omar Chong <al221811717@gmail.com>
2021-07-23 12:41:55 -0500

    Cambios en main

--- database/seeders/EstatusSeeder.php
+++ database/seeders/EstatusSeeder.php
@@ -7,30 +7,6 @@ use Illuminate\Support\Facades\DB;
 
 class EstatusSeeder extends Seeder
 {
-<<<<<<< HEAD
-    
-    public function run()
-    {
-        DB::table('estatus')->insert([
-            'nombre'=>'Activo',
-            'descripcion'=>'Este estatus estara activo siempre',
-            'created_at'=>date('Y-m-d H:i:s'),
-            'updated_at'=>date('Y-m-d H:i:s'),
-        ]);
-
-        DB::table('estatus')->insert([
-            'nombre'=>'Inactivo',
-            'descripcion'=>'Este estatus estara inactivo',
-            'created_at'=>date('Y-m-d H:i:s'),
-            'updated_at'=>date('Y-m-d H:i:s'),
-        ]);
-
-        DB::table('estatus')->insert([
-            'nombre'=>'Cancelado',
-            'descripcion'=>'Este estatus estara cancelado',
-            'created_at'=>date('Y-m-d H:i:s'),
-            'updated_at'=>date('Y-m-d H:i:s'),
-=======
 
     public function run()
     {
@@ -46,7 +22,6 @@ class EstatusSeeder extends Seeder
             'descripcion' => 'Este es el estatus que estara inactivo siempre',
             'created_at' => date('Y-m-d H:i:s'),
             'updated_at' => date('Y-m-d H:i:s'),
->>>>>>> c4bf0f29ede1f310a87ff41e9fcc7718d38cf412
         ]);
     }
 }
