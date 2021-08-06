c70440b4d238aaf568773e878f75883d82fb6cbc
Omar Chong <al221811717@gmail.com>
2021-08-06 12:12:57 -0500

    Modificacion de rutas

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,11 @@ Route::middleware(['auth'])->group(function () {
 
 Route::get('/', [InicioController::class,'Inicio'])->name('Inicio');
 Route::resource('productos', ProductosController::class);
+Route::name('pdfproductos')->get('pdfproductos',[ProductosController::class,'getPdfProductos']);
+Route::name('pdfcategorias')->get('pdfcategorias',[CategoriasController::class,'getPdfCategorias']);
+Route::name('pdfclientes')->get('pdfclientes',[ClientesController::class,'getPdfClientes']);
+Route::name('pdffases')->get('pdffases',[FasesController::class,'getPdfFases']);
+
 Route::resource('procesos', ProcesosController::class);
 Route::resource('clientes', ClientesController::class);
 Route::get('datatables/clientes', [ClientesController::class,'RegistrosDatatables'])->name('clientes.datatables');
@@ -30,7 +35,14 @@ Route::resource('materiales', MaterialesController::class);
 Route::resource('fases', FasesController::class);
 Route::resource('estatus', EstatusController::class);
 
-
-
+Route::get('datatables/estatus',[EstatusController::class,'RegistrosDatatables'])->name('estatus.datatables');
+Route::get('datatables/fases',[FasesController::class,'RegistrosDatatables'])->name('fases.datatables');
+Route::get('datatables/materiales',[MaterialesController::class,'RegistrosDatatables'])->name('materiales.datatables');
+Route::resource('Modulomateriales', MaterialVehiculoController::class);
+Route::post('materiales/search', [MaterialesController::class,'search'])->name('materiales.search');
+Route::post('Modulomateriales/search',[MaterialVehiculoController::class,'search'])->name('Modulomateriales.search');
+Route::post('Modulomateriales/buscarmaterial',[MaterialVehiculoController::class,'buscarmaterial'])->name('Modulomateriales.buscarmaterial');
+Route::post('Modulomateriales/buscarvehiculo',[MaterialVehiculoController::class,'buscarvehiculo'])->name('Modulomateriales.buscarvehiculo');
+Route::post('Modulomateriales/searchvehiculo',[MaterialVehiculoController::class,'searchvehiculo'])->name('Modulomateriales.searchvehiculo');
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 });
