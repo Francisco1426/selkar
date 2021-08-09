109bd94b3c082bf2fdda46d1e1912b71f50e789a
Omar Chong <al221811717@gmail.com>
2021-08-09 18:51:02 -0500

    Cambio seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -9,19 +9,11 @@ class DatabaseSeeder extends Seeder
 
     public function run()
     {
-<<<<<<< HEAD
-        $this->call(EstatusSeeder::class);
-=======
         $this->call(EstatuSeeder::class);
->>>>>>> 5a74de8ba5d0590c545e745e7372bef8d30dccfc
         $this->call(CategoriasSeeder::class);
         $this->call(ClienteSeeder::class);
         //$this->call(MaterialSeeder::class);
         $this->call(ProductosSeeder::class);
-<<<<<<< HEAD
-
-=======
->>>>>>> 5a74de8ba5d0590c545e745e7372bef8d30dccfc
         //$this->call(ProcesoSeeder::class);
 
         //$this->call(MaterialSeeder::class);
