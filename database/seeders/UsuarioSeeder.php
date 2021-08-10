c01e6550690cf838e24c92af880b25a6854ee553
Omar Chong <al221811717@gmail.com>
2021-08-09 19:31:16 -0500

    Seder actualizado

--- database/seeders/UsuarioSeeder.php
+++ database/seeders/UsuarioSeeder.php
@@ -7,12 +7,19 @@ use Illuminate\Support\Facades\DB;
 
 class UsuarioSeeder extends Seeder
 {
-    
+
     public function run()
     {
         DB::table('users')->insert([
             'name'=>'Selkar',
             'email'=>'al221810633@gmail.com',
+            'password'=>'selkar12345',
+            'created_at'=>date('Y-m-d H:i:s'),
+            'updated_at'=>date('Y-m-d H:i:s'),
+        ]);
+        DB::table('users')->insert([
+            'name'=>'Selkar',
+            'email'=>'al221811717@gmail.com',
             'password'=>'selkar12345',
             'created_at'=>date('Y-m-d H:i:s'),
             'updated_at'=>date('Y-m-d H:i:s'),
