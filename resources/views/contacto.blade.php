9aca72a168eafd65375b12e0ce602273dce5c3f8
Omar Chong <al221811717@gmail.com>
2021-08-12 16:04:51 -0500

    Diseño formulario de contactanos

--- resources/views/contacto.blade.php
+++ resources/views/contacto.blade.php
@@ -2,73 +2,93 @@
 
 <!-- inner page banner -->
 
-<div id="inner_banner" class="section inner_banner_section">
-  <div class="container">
-    <div class="row">
-      <div class="col-md-12">
-        <div class="full">
-          <div class="title-holder">
-            <div class="title-holder-cell text-left">
-              <h1 class="page-title">Contacto</h1>
-              <ol class="breadcrumb">
-                <li><a href="index.html">Inicio</a></li>
-                <li class="active">Contacto</li>
-              </ol>
+
+
+<div class="container col-md-10 mt-5">
+    <div class="card">
+        <div class="card-body">
+            <div class="card-title">
+                <h4 class="text-center">Contactanos</h4>
             </div>
-          </div>
+            <form method="post" action="{{ route('contact.store') }}">
+                @csrf
+                @if(Session::has('success'))
+                <div class="alert alert-success">
+                    {{Session::get('success')}}
+                </div>
+                @endif
+                <div class="form-row">
+                    <div class="col-md-6">
+                        <label for="nombre" class="col-sm-1-12 col-form-label">Nombre completo</label>
+                        <div class="form-group">
+                            <input type="text" class="form-control" name="name" id="name">
+                            @error('name')
+                            <small class="text-danger">{{ $message }}</small>
+                            @enderror
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <label for="nombre" class="col-sm-1-12 col-form-label">Email</label>
+                        <div class="form-group">
+                            <input type="email" class="form-control" name="email" id="email">
+                            @error('email')
+                            <small class="text-danger">{{ $message }}</small>
+                            @enderror
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <label for="nombre" class="col-sm-1-12 col-form-label">Telefono</label>
+                        <div class="form-group">
+                            <input type="text" class="form-control" name="phone" id="phone">
+                            @error('phone')
+                            <small class="text-danger">{{ $message }}</small>
+                            @enderror
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <label for="nombre" class="col-sm-1-12 col-form-label">Asunto</label>
+                        <div class="form-group">
+                            <input type="text" class="form-control" name="subject" id="subject">
+                            @error('subject')
+                            <small class="text-danger">{{ $message }}</small>
+                            @enderror
+                        </div>
+                    </div>
+                    <div class="col-md-12">
+                        <label for="nombre" class="col-sm-1-12 col-form-label">Mensaje</label>
+                        <div class="form-group">
+                            <textarea class="form-control" name="message" id="message" rows="4"></textarea>
+                            @error('message')
+                            <small class="text-danger">{{ $message }}</small>
+                            @enderror
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-12">
+                    <input type="submit" name="send" value="Enviar" class="btn btn-success">
+                </div>
+            </form>
         </div>
-      </div>
     </div>
-  </div>
 </div>
 <br>
-<br>
-
-
-    <div class="container mt-5">
-
-        <form method="post" action="{{ route('contact.store') }}">
-            
-
-            <!-- CROSS Site Request Forgery Protection -->
-            @csrf
-            <center><h2>Formulario de Contacto</h2></center>
-              <!-- Success message -->
-        @if(Session::has('success'))
-            <div class="alert alert-success">
-                {{Session::get('success')}}
+<div id="inner_banner" class="section inner_banner_section">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="full">
+                    <div class="title-holder">
+                        <div class="title-holder-cell text-left">
+                            <h1 class="page-title">Contacto</h1>
+                            <ol class="breadcrumb">
+                                <li><a href="index.html">Inicio</a></li>
+                                <li class="active">Contacto</li>
+                            </ol>
+                        </div>
+                    </div>
+                </div>
             </div>
-        @endif
-
-            <div class="form-group">
-                <label><b>Nombre Completo :</b></label>
-                <input type="text" class="form-control" name="name" id="name">
-            </div>
-
-            <div class="form-group">
-                <label><b>Email :</b></label>
-                <input type="email" class="form-control" name="email" id="email">
-            </div>
-
-            <div class="form-group">
-                <label><b>Telefono :</b></label>
-                <input type="text" class="form-control" name="phone" id="phone">
-            </div>
-
-            <div class="form-group">
-                <label><b>Asunto :</b></label>
-                <input type="text" class="form-control" name="subject" id="subject">
-            </div>
-
-            <div class="form-group">
-                <label><b>Mensaje :</b></label>
-                <textarea class="form-control" name="message" id="message" rows="4"></textarea>
-            </div>
-
-            <input type="submit" name="send" value="Enviar" class="btn btn-success btn-block">
-        </form>
+        </div>
     </div>
-    <br>
-
-
+</div>
 @include ('vista2')
--- resources/views/system/estatus/create.blade.php
+++ resources/views/system/estatus/create.blade.php
@@ -3,61 +3,59 @@
 @section('css')
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/trix/1.3.1/trix.css">
 @endsection
-    <div class="main">
-        <!-- MAIN CONTENT -->
-        <div class="main-content">
-            <div class="container-fluid">
-                <!-- OVERVIEW -->
-                <div class="panel panel-headline">
-                    <div class="panel-heading">
-                        <h3 class="panel-title">Alta Estatus</h3>
-                        <p class="panel-subtitle">Llena todos los campos</p>
-                    </div>
-                    <div class="panel-body">
-                    
-
+<div class="main">
+    <!-- MAIN CONTENT -->
+    <div class="main-content">
+        <div class="container-fluid">
+            <!-- OVERVIEW -->
             <div class="panel panel-headline">
                 <div class="panel-heading">
-                    <h3 class="panel-title">Alta de Clientes</h3>
-                    <p class="panel-subtitle">Registro de clientes</p>
+                    <h3 class="panel-title">Alta Estatus</h3>
+                    <p class="panel-subtitle">Llena todos los campos</p>
                 </div>
                 <div class="panel-body">
+                    <div class="panel panel-headline">
+                        <div class="panel-heading">
+                            <h3 class="panel-title">Alta de Clientes</h3>
+                            <p class="panel-subtitle">Registro de clientes</p>
+                        </div>
+                        <div class="panel-body">
 
-                    <form action="{{ route('estatus.store')}}" method="POST">
-                        @csrf
-                        <div class="form-row">
-                            <div class="col-md-12">
-                                <label for="nombre" class="col-sm-1-12 col-form-label">Estatus</label>
-                                <div class="form-group">
-                                    <input type="text" class="form-control @error('nombre') is-invalid @enderror" name="nombre" id="nombre" value="{{old('nombre')}}" placeholder="Escriba el nombre....">
-                                </div>
-                                @error('nombre')
-                                <small class="text-danger">{{ $message }}</small>
-                                @enderror
-                            </div>
-                            <div class="col-md-12">
-                                <label for="nombre" class="col-sm-1-12 col-form-label">Descripcion</label>
-                                <div class="form-group">
-                                    <input type="text" class="form-control @error('descripcion') is-invalid @enderror" name="descripcion" id="descripcion" value="{{old('descripcion')}}" placeholder="Escriba el nombre....">
-                                    <trix-editor input="descripcion"></trix-editor>
-                                </div>
-                                @error('descripcion')
-                                <small class="text-danger">{{ $message }}</small>
-                                @enderror
-                            </div>
-                            
-                            <div class="col-md-11">
-                                <div class="form-group">
-                                    <button type="submit" class="btn btn-success">Guardar</button>
-                                </div>
-                            </div>
-                        </form>
+                            <form action="{{ route('estatus.store')}}" method="POST">
+                                @csrf
+                                <div class="form-row">
+                                    <div class="col-md-12">
+                                        <label for="nombre" class="col-sm-1-12 col-form-label">Estatus</label>
+                                        <div class="form-group">
+                                            <input type="text" class="form-control @error('nombre') is-invalid @enderror" name="nombre" id="nombre" value="{{old('nombre')}}" placeholder="Escriba el nombre....">
+                                        </div>
+                                        @error('nombre')
+                                        <small class="text-danger">{{ $message }}</small>
+                                        @enderror
+                                    </div>
+                                    <div class="col-md-12">
+                                        <label for="nombre" class="col-sm-1-12 col-form-label">Descripcion</label>
+                                        <div class="form-group">
+                                            <input type="text" class="form-control @error('descripcion') is-invalid @enderror" name="descripcion" id="descripcion" value="{{old('descripcion')}}" placeholder="Escriba el nombre....">
+                                            <trix-editor input="descripcion"></trix-editor>
+                                        </div>
+                                        @error('descripcion')
+                                        <small class="text-danger">{{ $message }}</small>
+                                        @enderror
+                                    </div>
+
+                                    <div class="col-md-11">
+                                        <div class="form-group">
+                                            <button type="submit" class="btn btn-success">Guardar</button>
+                                        </div>
+                                    </div>
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-@section('js')
-<script src="https://cdnjs.cloudflare.com/ajax/libs/trix/1.3.1/trix.js"></script>
-@endsection
-@endsection
+        @section('js')
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/trix/1.3.1/trix.js"></script>
+        @endsection
+        @endsection
