df06d78144420fa6427129f4e662c07bc7db4a00
Omar Chong <al221811717@gmail.com>
2021-08-12 13:47:37 -0500

    Pdf configuracion

--- resources/views/system/productos/pdf.blade.php
+++ resources/views/system/productos/pdf.blade.php
@@ -6,8 +6,50 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="{{asset('css/tabla.css')}}">
     <title>Reporte de productos</title>
+    <style>
+        body{
+    font-family: Arial;
+}
+#main-container{
+margin:150px auto;
+width: 600px;
+}
+
+table{
+background-color:white;
+text-align: left;
+border-collapse: collapse;
+width: 100%;
+}
+
+th, td{
+
+padding: 5px;
+}
+thead{
+background-color: #232f3e;
+border-bottom: solid 5px #232f3e;
+color: white;
+}
+tr:nth-child(even){
+background-color: #ddd;
+}
+
+tr:hover td{
+background-color: #369681;
+color: white;
+}
+
+h2{
+    color: 000000;
+     font-size: 20px;
+     font-family: Arial;
+     text-decoration-line: underline;
+    padding: 20px;
+   }
+
+    </style>
 </head>
 <body>
     <div class="container">
